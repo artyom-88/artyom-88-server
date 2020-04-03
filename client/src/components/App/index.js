2df67e4b78d3074f6d9850b59311553a6ee1f9bc
Artem_Ganev <a.a.ganev@gmail.com>
2020-04-03 15:39:15 +0300

    fix codebeat issues

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,18 +1,14 @@
-import React, {useEffect} from 'react';
-import {shallowEqual, useSelector} from 'react-redux';
-import {useHistory, useLocation} from 'react-router';
-import {Redirect, Route, Switch} from 'react-router-dom';
-import {appSelector} from '../../selectors';
-import Career from '../Career';
+import React, { useEffect } from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
+import { useHistory, useLocation } from 'react-router';
+import { appSelector } from '../../selectors';
 import Login from '../Login';
-import Main from '../Main';
-import Menu from '../Menu';
-import NotFound from '../NotFound';
+import Routes from '../Routes';
 
 const LOGIN_PATH = '/login';
 
 const App = () => {
-  const {authorized} = useSelector(appSelector, shallowEqual);
+  const { authorized } = useSelector(appSelector, shallowEqual);
   const history = useHistory();
   const location = useLocation();
 
@@ -22,30 +18,7 @@ const App = () => {
     }
   }, [history, location, authorized]);
 
-  return (
-    <div className='container-xl flexBox flexColumn flexGrow-1'>
-      {authorized
-        ? (
-          <div className='flexBox flexColumn'>
-            <Menu/>
-            <div className='flexBox flexColumn flexGrow-1'>
-              <Switch>
-                <Redirect from='/main' to='/'/>
-                <Route exact={true} path='/' component={Main}/>
-                <Route exact={true} path='/career' component={Career}/>
-                <Route exact={true} path='/login' component={Login}/>
-                <Route component={NotFound}/>
-              </Switch>
-            </div>
-          </div>
-        ) : (
-          <Switch>
-            <Login/>
-          </Switch>
-        )
-      }
-    </div>
-  );
+  return <div className='container-xl flexBox flexColumn flexGrow-1'>{authorized ? <Routes /> : <Login />}</div>;
 };
 
 export default App;
