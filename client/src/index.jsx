f9ccbb7d9b92e81016f64a8404a6b2b7317e3bdf
Artem_Ganev <a.a.ganev@gmail.com>
2020-04-06 14:38:41 +0300

    add history

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,26 +1,21 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { useHistory } from 'react-router';
-import { HashRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import './assets/styles.scss';
 import App from './components/App';
 import configStore from './store';
 
-const HistoryWrapper = () => {
-  const history = useHistory();
-  return (
-    <Provider store={configStore(history)}>
-      <App />
-    </Provider>
-  );
-};
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <HistoryWrapper />
-    </HashRouter>
+    <Provider store={configStore(history)}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
