a071ca636d626a033e029e97e50a4854f15e3c4d
Artem_Ganev <a.a.ganev@gmail.com>
2021-01-12 10:47:29 +0300

    fix env variable name for Heroku

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  await app.listen(process.env.APP_PORT || 3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
