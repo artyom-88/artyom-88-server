332fe19db6e2aaf2e09e2447e690cc32448682a8
Artem Ganev <a.a.ganev@gmail.com>
2023-11-24 16:23:00 -0500

    Add API_URL env variable support

--- src/common/common-constants.ts
+++ src/common/common-constants.ts
@@ -1,10 +1,13 @@
+import * as process from 'process';
+
 export const isServer = typeof window === 'undefined';
 export const isDev = process.env.NODE_ENV === 'development';
 export const PROTOCOL = (isServer ? global.location?.protocol : window.location?.protocol) ?? 'http';
 export const HOST = (isServer ? global.location?.hostname : window.location?.hostname) ?? 'localhost';
 export const PORT = process.env.PORT || 3000;
 // protocol comes with ":" on client side
-export const API_URL = isServer ? `${PROTOCOL}://${HOST}:${PORT}/api` : `${PROTOCOL}//${HOST}:${PORT}/api`;
+export const API_URL =
+  process.env.API_URL ?? (isServer ? `${PROTOCOL}://${HOST}:${PORT}/api` : `${PROTOCOL}//${HOST}:${PORT}/api`);
 // routes
 export const BLOG_PAGE_ID = 'blog';
 export const BLOG_PAGE_URL = `/${BLOG_PAGE_ID}`;
@@ -19,5 +22,6 @@ if (isDev) {
   console.log('PROTOCOL', PROTOCOL);
   console.log('HOST', HOST);
   console.log('PORT', PORT);
-  console.log('API_URL', API_URL);
 }
+
+console.log('API_URL', API_URL);
